f602042d90323dd49f31cc536b54f9e1c68523b2
joaocosta <joaocosta@zonalivre.org>
2009-11-07 22:26:08 -0500

    Add simple indicator viewer.

--- lib/Finance/HostedTrader/UI/Controller/Signal.pm
+++ lib/Finance/HostedTrader/UI/Controller/Signal.pm
@@ -3,6 +3,9 @@ package Finance::HostedTrader::UI::Controller::Signal;
 use strict;
 use warnings;
 use parent 'Catalyst::Controller';
+use Finance::HostedTrader::Datasource;
+use Finance::HostedTrader::ExpressionParser;
+use JSON;
 
 =head1 NAME
 
@@ -23,12 +26,56 @@ Catalyst Controller.
 
 sub signal : Global {
     my ( $self, $c, @args ) = @_;
+    $c->forward('View::TT');
+}
 
+sub parse :Local {
+    my ( $self, $c, @args ) = @_;
+    my $db = Finance::HostedTrader::Datasource->new();
+    my $signal_processor = Finance::HostedTrader::ExpressionParser->new($db);
+    my $args = $c->request->query_params;
+
+    my $timeframe  = $args->{'t'} || 'day';
+    my $expr = $args->{'e'};
+
+    $c->response->content_type('application/json');
+
+    my ($max_loaded_items, $max_display_items, $symbols_txt) = (1000, 1);
+    my $symbols = $db->getNaturalSymbols;
+    my @results;
+    foreach my $symbol (@{$symbols}) {
+        my $data = $signal_processor->getIndicatorData({ 
+								    'expr'   => $expr, 
+								    'symbol' => $symbol, 
+								    'tf'     => $timeframe, 
+								    'maxLoadedItems' => $max_loaded_items, 
+								    'maxDisplayItems' => $max_display_items,
+                                });
+        next unless(defined($data));
+        $data = $data->[0];
+        next unless(defined($data));
+
+        my %hash;
+        $hash{symbol} = $symbol;
+        for (my $i=0;$i<scalar(@$data);$i++) {
+            $hash{"item$i"} = $data->[$i];
+        }
+
+        push @results, \%hash;
+    }
+
+    my $obj = {
+        "ResultSet" => {
+            "Total" => scalar(@results),
+            "Result" => \@results,
+        }
+    };
+
+    $c->response->write(to_json($obj));
 }
 
 sub end : Private {
     my ( $self, $c ) = @_;
-    $c->forward('View::TT');
 }
 
 
