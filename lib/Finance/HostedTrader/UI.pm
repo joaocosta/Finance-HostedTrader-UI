f041343f2a184c0e788fd66ca520850951ec5ebf
João Costa <joaocosta@zonalivre.org>
2015-03-05 23:41:45 +0000

    update version number for new release

--- lib/Finance/HostedTrader/UI.pm
+++ lib/Finance/HostedTrader/UI.pm
@@ -17,7 +17,7 @@ use parent qw/Catalyst/;
 use Catalyst qw/-Debug
                 ConfigLoader
                 Static::Simple/;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 # Configure the application.
 #
